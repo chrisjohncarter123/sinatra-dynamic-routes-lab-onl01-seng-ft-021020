aef957ad75f9726df3335787578a0d4ee1e943c0
Christopher Carter <chrisjohncarter123@gmail.com>
2020-03-24 17:35:43 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -12,13 +12,8 @@ class App < Sinatra::Base
   end
   
   get '/say/:number/:phrase' do
-    result = ''
-    number = params[:number].to_i
-    number.each do
-      result << params[:phrase] 
-      result << " "
-    end
-    return result
+    
+    return params[:phrase] * params[:number].to_i
   end
   
   get '/say/:word1/:word2/:word3/:word4/:word5' do
